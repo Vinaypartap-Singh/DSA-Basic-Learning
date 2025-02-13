f82b5c81803c2fcfc9a963263b4a9ff5ccc7dcd5
Vinaypartap Singh <developervsandhu@gmail.com>
2025-02-13 12:34:04 -0500

    Tree Traversal

--- BInarySearchTree.js
+++ BInarySearchTree.js
@@ -53,4 +53,57 @@ class BinarySearchTree {
 
     return false;
   }
+
+  bfs() {
+    let node = this.root;
+    let data = [];
+    let queue = [];
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
+
+  DSFPreOrder() {
+    let data = [];
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPostOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+
+    traverse(this.root);
+    return data;
+  }
+
+  DfsInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+
+    traverse(this.root);
+    return data;
+  }
 }
